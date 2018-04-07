2f447a5e0ee0cc8c7e111a8929e5071a81a7217e
Barichello <arturbarichello@hotmail.com>
2018-04-07 17:02:28 -0300

    Update adder.vhd

--- 1/adder.vhd
+++ 1/adder.vhd
@@ -7,21 +7,21 @@ USE ieee.std_logic_signed.all;
 ENTITY adder IS
 PORT (a, b     : IN  SIGNED(15 DOWNTO 0);
       overflow : OUT STD_LOGIC;
-	  s        : OUT SIGNED(15 DOWNTO 0));
+      s        : OUT SIGNED(15 DOWNTO 0));
 END adder;
 
 ARCHITECTURE bhv OF adder IS
-SIGNAL sum: SIGNED(16 DOWNTO 0);
+SIGNAL locala, localb, sum: SIGNED(16 DOWNTO 0); -- One bit more than input
+SIGNAL localflow: STD_LOGIC;
+SIGNAL sumout: SIGNED(16 DOWNTO 0);
 
 BEGIN
-	PROCESS(sum)
-	BEGIN
-		sum <= a + b;
-		IF sum(16) = '1' THEN
-			overflow <= '1';
-		ELSE
-			overflow <= '0';
-			s <= sum;
-		END IF;
-	END PROCESS;
+	locala <= RESIZE((a), 17);
+	localb <= RESIZE((b), 17);
+	sum <= locala + localb;
+	--overflow occurs when bit 16 is not equal to sign bit 15
+	localflow <= '1' when sum(15) /= sum(4);
+	sumout <= SIGNED(sum);
+	overflow <= localflow;
+	s <= sumout(16)&sum(14 DOWNTO 0); 
 END bhv;
