9b249189c22dbe731180a85b0b0905f87577e04c
aBARICHELLO <arturbarichello@hotmail.com>
2018-04-26 11:11:57 -0300

    Folder structure

---
+++ components/registers/generic_register/register.vhd
@@ -0,0 +1,23 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+
+ENTITY register IS
+GENERIC (n: INTEGER := 8);
+PORT (clk, reset, load : IN STD_LOGIC;
+      d                : IN STD_LOGIC_VECTOR(n-1 DOWNTO 0);
+      q                : OUT STD_LOGIC_VECTOR(n-1 DOWNTO 0));
+END register;
+
+ARCHITECTURE bhv OF register IS
+BEGIN
+    PROCESS(clk)
+    BEGIN
+        IF (reset = '0') THEN
+            q <= (OTHERS => '0');
+        ELSIF (rising_edge(clk)) THEN
+            IF (load = '1') THEN
+                q <= d;
+            END IF;
+        END IF;
+    END PROCESS;
+END bhv;
